7ded3530e7c95de48bd862634673967c996a6712
Sonita <sonita.borin2@gmail.com>
2025-01-18 15:36:26 +0700

    initialize project

---
+++ ColorAdmin/Areas/HR/Controllers/EmployeeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ColorAdmin.Areas.HR.Controllers
+{
+	[Area("HR")]
+	public class EmployeeController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+		public IActionResult New()
+		{
+			return View();
+		}
+	}
+}
